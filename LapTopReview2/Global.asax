d8a6dd8fc3f2030d5aad7316ea9f4b7169594b15
pfittante <pfittante@yahoo.com>
2017-07-28 11:51:53 -0400

    inital commit

---
+++ LapTopReview2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LapTopReview2.MvcApplication" Language="C#" %>
---
+++ LapTopReview2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LapTopReview2.Startup))]
+namespace LapTopReview2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
