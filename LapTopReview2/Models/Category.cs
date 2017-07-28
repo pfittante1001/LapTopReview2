d8617e11565f70ac1cdfb7b784d755c5cc3409e0
pfittante <pfittante@yahoo.com>
2017-07-28 11:57:19 -0400

    models complete

---
+++ LapTopReview2/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LapTopReview2.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int CategoryID { get; set; }
+        public string Brand { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
